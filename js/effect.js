207e5814427c5b775f8a16fa8332aab560ea1a88
airititim <airititim0816@gmail.com>
2020-02-15 17:17:15 +0800

    test

--- js/effect.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    var WebHeight = document.body.clientHeight;
-    var WindowScreen = window.screen.height;
-    var BodyHeight = document.body.clientHeight;
-    var divHei = $('div').height();
-    console.log(WebHeight + '&' + WindowScreen + '&' + BodyHeight + '&' + divHei);
-});
