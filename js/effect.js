4c20a467130ddc3db84ade958c7a0208823eb610
airititim <airititim0816@gmail.com>
2020-03-20 10:22:03 +0800

    test

--- js/effect.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.open').click(function (e) { 
-        $('.pop').fadeIn(500);
-    });
-    $('.close').click(function (e) { 
-        $('.pop').fadeOut(500);
-    });
-});
